aa345e1611b3131391c622ab3d24d7726aba5744
Azzaz khan <students.mails9@gmail.com>
2021-09-01 01:06:17 +0500

    Added titles to each route view and fiexed banner link

--- src/components/header/Banner.vue
+++ src/components/header/Banner.vue
@@ -1,52 +1,52 @@
 <template>
   <div class="angled-banner">
-    <router-link to="/#news">New Tor Onion V3 URL</router-link>
+    <router-link to="#news">New Tor Onion V3 URL</router-link>
   </div>
 </template>
 
 <style lang="scss">
-.angled-banner {
-  position: absolute;
-  color: white !important;
-  background-color: var(--color-primary);
-  top: 45px;
-  left: -65px;
-  font-size: 1.05rem;
-  font-weight: bold;
-  text-align: center;
-  text-transform: uppercase;
-  text-decoration: none;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 250px;
-  padding: 0;
-  line-height: 1.1em;
-  transform: rotate(-45deg);
-  transition: transform 0.2s ease;
-  &:hover {
-    transform: rotate(-45deg) scale(1.1);
-  }
-  & > * {
+  .angled-banner {
+    position: absolute;
     color: white !important;
-    text-decoration: none !important;
-    width: 140px;
-  }
-  @media (min-width: 1100px) {
-    width: 350px;
-    top: 80px;
-    left: -70px;
-    height: 2.4rem;
+    background-color: var(--color-primary);
+    top: 45px;
+    left: -65px;
+    font-size: 1.05rem;
+    font-weight: bold;
+    text-align: center;
+    text-transform: uppercase;
+    text-decoration: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 250px;
+    padding: 0;
+    line-height: 1.1em;
+    transform: rotate(-45deg);
+    transition: transform 0.2s ease;
+    &:hover {
+      transform: rotate(-45deg) scale(1.1);
+    }
     & > * {
-      width: 100%;
+      color: white !important;
+      text-decoration: none !important;
+      width: 140px;
+    }
+    @media (min-width: 1100px) {
+      width: 350px;
+      top: 80px;
+      left: -70px;
+      height: 2.4rem;
+      & > * {
+        width: 100%;
+      }
     }
   }
-}
 </style>
 
 <script lang="ts">
-import Vue from "vue";
-export default Vue.extend({
-  name: "Banner",
-});
+  import Vue from "vue";
+  export default Vue.extend({
+    name: "Banner"
+  });
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,8 @@ import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
+const DEFAULT_TITLE = "FoxMixer - High Quality Bitcoin Mixer";
+
 const routes: Array<RouteConfig> = [
   {
     path: "/",
@@ -13,52 +15,62 @@ const routes: Array<RouteConfig> = [
   {
     path: "/mixer",
     name: "Mixer",
-    component: () => import(/* webpackChunkName: "mixer" */ "../views/Mixer.vue")
+    component: () => import(/* webpackChunkName: "mixer" */ "../views/Mixer.vue"),
+    meta: {title: "FoxMixer - Start"}
   },
   {
     path: "/tutorial",
     name: "Tutorial",
-    component: () => import(/* webpackChunkName: "tutorial" */ "../views/Tutorial.vue")
+    component: () => import(/* webpackChunkName: "tutorial" */ "../views/Tutorial.vue"),
+    meta: {title: "FoxMixer - Tutorial"}
   },
   {
     path: "/faq",
     name: "FAQ",
-    component: () => import(/* webpackChunkName: "faq" */ "../views/FAQ.vue")
+    component: () => import(/* webpackChunkName: "faq" */ "../views/FAQ.vue"),
+    meta: {title: "FoxMixer - FAQ"}
   },
   {
     path: "/invest",
     name: "Invest",
-    component: () => import(/* webpackChunkName: "invest" */ "../views/Invest.vue")
+    component: () => import(/* webpackChunkName: "invest" */ "../views/Invest.vue"),
+    meta: {title: "FoxMixer - Invest"}
   },
   {
     path: "/contact",
     name: "Contact",
-    component: () => import(/* webpackChunkName: "contact" */ "../views/Contact.vue")
+    component: () => import(/* webpackChunkName: "contact" */ "../views/Contact.vue"),
+    meta: {title: "FoxMixer - Contact"}
   },
   {
     path: "/opengpg",
     name: "OpenPGP",
-    component: () => import(/* webpackChunkName: "openpgp" */ "../views/OpenGPG.vue")
+    component: () => import(/* webpackChunkName: "openpgp" */ "../views/OpenGPG.vue"),
+    meta: {title: "FoxMixer - OpenPGP Public Key"}
   },
   {
     path: "/reviews",
     name: "Reviews",
-    component: () => import(/* webpackChunkName: "reviews" */ "../views/Reviews.vue")
+    component: () => import(/* webpackChunkName: "reviews" */ "../views/Reviews.vue"),
+    meta: {title: "FoxMixer - Reviews"}
   },
   {
     path: "/api",
     name: "API",
-    component: () => import(/* webpackChunkName: "api" */ "../views/API.vue")
+    component: () => import(/* webpackChunkName: "api" */ "../views/API.vue"),
+    meta: {title: "FoxMixer - Developer API"}
   },
   {
     path: "/mix/:mixCode/complete",
     name: "MixComplete",
-    component: () => import(/* webpackChunkName: "mixComplete" */ "../views/MixComplete.vue")
+    component: () => import(/* webpackChunkName: "mixComplete" */ "../views/MixComplete.vue"),
+    meta: {title: "FoxMixer - Your Mix is Ready!"}
   },
   {
     path: "/mix/:mixCode",
     name: "MixStatus",
-    component: () => import(/* webpackChunkName: "mixStatus" */ "../views/Status.vue")
+    component: () => import(/* webpackChunkName: "mixStatus" */ "../views/Status.vue"),
+    meta: {title: "FoxMixer - Status of Mix"}
   }
 ];
 
@@ -66,6 +78,14 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+router.afterEach((to) => {
+  // Use next tick to handle router history correctly
+  // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
+  Vue.nextTick(() => {
+    document.title = to?.meta?.title || DEFAULT_TITLE;
+  });
 });
 
 export default router;
